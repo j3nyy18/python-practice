e4150bea1a5416a432bacaec408a94dfe51fa392
j3ny18 <22amtics205@gmail.com>
2026-08-19 16:46:27 +0530

    Python Conditional Statements

--- task5.py
+++ task5.py
@@ -77,8 +77,17 @@ if username == "admin" and password == "12345":
 else:
     print("Invalid username or password!! Try Again later!")
 
+
 # Electricity bill calculator based on units consumed.
 
+units = float(input("Enter units consumed: "))
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+print("Electricity Bill: ", bill)
 
 # Simple calculator (add, subtract, multiply, divide).
 
